b05c7348235ecb57fd719eb92c49c469d836c4a9
Signeus <kev.ms93@gmail.com>
2018-11-14 22:41:07 +0100

    Raycast and Wall Colliders

---
+++ Assets/Scripts/_Raycast/MyRaycastSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRaycastSelect : RaycastSelect {
+
+    protected override void OnRaycastEnter(GameObject target)
+    {
+        print("Activated trigger");
+    }
+    protected override void OnRaycastLeave(GameObject target)
+    {
+        print("Hey ya no me miras!");
+    }
+
+    protected override void UpdateFromFather()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * selectionDistance;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+}
---
+++ Assets/Scripts/_Raycast/RaycastSelect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class RaycastSelect : MonoBehaviour {
+
+    public float selectionDistance = 1f;
+    public LayerMask layerMask;
+
+    private GameObject currentTarget;
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, selectionDistance, layerMask))
+        {
+            if(currentTarget == null)
+            {
+                currentTarget = hit.transform.gameObject;
+                OnRaycastEnter(currentTarget);
+            }
+            else if (currentTarget != hit.transform.gameObject)
+            {
+                OnRaycastLeave(currentTarget);
+                currentTarget = hit.transform.gameObject;
+                OnRaycastEnter(currentTarget);
+            }
+
+            OnRaycast(hit.transform.gameObject);
+        }
+        else if (currentTarget != null)
+        {
+            OnRaycastLeave(currentTarget);
+            currentTarget = null;
+        }
+
+        UpdateFromFather();
+    }
+
+    protected virtual void OnRaycastEnter(GameObject target)
+    {
+
+    }
+
+    protected virtual void OnRaycastLeave(GameObject target)
+    {
+
+    }
+
+    protected virtual void OnRaycast(GameObject target)
+    {
+
+    }
+
+    protected virtual void UpdateFromFather()
+    {
+
+    }
+}
