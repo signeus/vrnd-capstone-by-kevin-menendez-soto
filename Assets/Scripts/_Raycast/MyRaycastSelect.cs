a71b44d9d460592c83025f290ba3597c5022bd55
Signeus <kev.ms93@gmail.com>
2018-11-15 18:45:51 +0100

    Improved enemy behavior

--- Assets/Scripts/_Raycast/MyRaycastSelect.cs
+++ Assets/Scripts/_Raycast/MyRaycastSelect.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class MyRaycastSelect : RaycastSelect
 {
+    bool detected = false;
+    GameObject enemy;
+
+    float rotateSpeed = 5f;
+
     protected override void OnRaycastEnter(GameObject target)
     {
         gm.GameOver();
-        print("Activated trigger");
+        enemy = target;
+        detected = true;
     }
     protected override void OnRaycastLeave(GameObject target)
     {
-        print("Hey ya no me miras!");
+
     }
 
     protected override void UpdateFromFather()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward) * selectionDistance;
         Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (detected)
+        {
+            Vector3 direction = Vector3.right * rotateSpeed;
+            enemy.transform.Rotate(direction * Time.deltaTime);
+        };
     }
 }
